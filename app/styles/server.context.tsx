07651920a52151293a3e9fb270c0748d9c28e152
Cora Sutton <cora@sutton.me>
2022-02-24 19:38:05 -0600

    First!!

---
+++ app/styles/server.context.tsx
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+
+export interface ServerStyleContextData {
+  key: string;
+  ids: Array<string>;
+  css: string;
+}
+
+const ServerStyleContext = createContext<null | ServerStyleContextData[]>(
+  null
+);
+
+export default ServerStyleContext;
